8286dfd9f77c03e98f2ae7c8a83f78694756c5da
jen3326 <kisss44@naver.com>
2021-08-31 09:10:46 +0900

    add

--- src/chap01/sec03/exam01/Hello.java
+++ src/chap01/sec03/exam01/Hello.java
@@ -1,12 +1,7 @@
 package chap01.sec03.exam01;
-/* 범위 주석
-작성자: 김자바
-작성일: 2021.08.31
- */
+
 public class Hello {
-    // 프로그램 실행 진입점
     public static void main(String[] args) {
-        // 콘솔에 출력하는 실행문
         System.out.println("Hello, Java");
-    } // end of main
-} // end of class
+    }
+}
