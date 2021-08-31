89663f93baa75a48ec27d1b66b058f871dd084a3
jen3326 <kisss44@naver.com>
2021-08-31 16:54:26 +0900

    add

---
+++ src/chap04/sec02/exam09/BreakOutterExample.java
@@ -0,0 +1,17 @@
+package chap04.sec02.exam09;
+
+// 바깥쪽 반복문 종료
+public class BreakOutterExample {
+    public static void main(String[] args) {
+        Outter: // 라벨
+        for (char upper = 'A'; upper <= 'Z'; upper++) {
+            for (char lower = 'a'; lower <= 'z'; lower++) {
+                System.out.println(upper + "-" + lower);
+                if (lower == 'g') {
+                    break Outter;
+                }
+            }
+        }
+        System.out.println("프로그램 실행 종료");
+    }
+}
