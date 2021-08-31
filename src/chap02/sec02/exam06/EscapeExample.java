7c80af743e95cea61419bbfdcfab6e2f8cf280b7
jen3326 <kisss44@naver.com>
2021-08-31 10:23:07 +0900

    add

---
+++ src/chap02/sec02/exam06/EscapeExample.java
@@ -0,0 +1,12 @@
+package chap02.sec02.exam06;
+
+// 이스케이프 문자 출력
+public class EscapeExample {
+    public static void main(String[] args) {
+        System.out.println("번호\t이름\t직업");
+        System.out.println("행 단위 출력\n");
+        System.out.println("행 단위 출력\n");
+        System.out.println("우리는 \"개발자\" 입니다.");
+        System.out.println("봄\\여름\\가을\\겨울");
+    }
+}
