82ef478366c357d566d5ea8a8f5b2c67c6625e1d
jen3326 <kisss44@naver.com>
2021-08-31 12:08:00 +0900

    add

---
+++ src/chap02/sec03/exam03/ByteOperationExample.java
@@ -0,0 +1,14 @@
+package chap02.sec03.exam03;
+
+// 정수 타입의 연산
+public class ByteOperationExample {
+    public static void main(String[] args) {
+        byte result1 = 10 + 20;
+        System.out.println(result1);
+
+        byte x = 10;
+        byte y = 20;
+        int result2 = x + y;
+        System.out.println(result2);
+    }
+}
---
+++ src/chap02/sec03/exam07/PrimitiveAndStringConversionExample.java
@@ -0,0 +1,22 @@
+package chap02.sec03.exam07;
+
+// 기본 타입과 문자열 간의 변환
+public class PrimitiveAndStringConversionExample {
+    public static void main(String[] args) {
+        int value1 = Integer.parseInt("10"); // 문자열을 기본타입으로
+        double value2 = Double.parseDouble("3.14");
+        boolean value3 = Boolean.parseBoolean("true");
+
+        System.out.println("value1: " + value1);
+        System.out.println("value2: " + value2);
+        System.out.println("value3: " + value3);
+
+        String str1 = String.valueOf(10); // 기본 타입을 문자열로
+        String str2 = String.valueOf(3.14);
+        String str3 = String.valueOf(true);
+
+        System.out.println("str1: " + str1);
+        System.out.println("str2: " + str2);
+        System.out.println("str3: " + str3);
+    }
+}
